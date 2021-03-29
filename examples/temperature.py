8e8786b415decb4972c10fa72ab59003b16c149c
Ciprian Manea <ciprian.manea@gmail.com>
2021-03-29 11:21:17 +0300

    Create temperature.py

---
+++ examples/temperature.py
@@ -0,0 +1,20 @@
+import micropython
+import machine
+import utime
+
+# from https://github.com/raspberrypi/pico-micropython-examples/blob/master/adc/temperature.py
+
+sensor_temp = machine.ADC(4)
+conversion_factor = micropython.const(3.3 / 65535)
+
+while True:
+    reading = sensor_temp.read_u16() * conversion_factor
+    
+    # The temperature sensor measures the Vbe voltage of a biased bipolar diode, connected to the fifth ADC channel
+    # Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree. 
+    temperature = 27 - (reading - 0.706) / 0.001721
+    
+    # interestingly, Pico has an up-to-date realtime clock
+    print(temperature, "C degrees at", utime.localtime())
+    
+    utime.sleep(2)
