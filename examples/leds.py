c4db6c4e9ee412e7e5f310cbdc67c0c776a3720e
Ciprian Manea <ciprian.manea@gmail.com>
2021-04-02 16:33:06 +0300

    Update leds.py

--- examples/leds.py
+++ examples/leds.py
@@ -1,11 +1,11 @@
 from machine import Pin, Timer
 
 # Red on the tiny Pico
-led = Pin(18, Pin.OUT)
+rgbRed = Pin(18, Pin.OUT)
 # Green on the tiny Pico
-led = Pin(19, Pin.OUT)
+rgbGreen = Pin(19, Pin.OUT)
 # Blue on the tiny Pico
-led = Pin(20, Pin.OUT)
+rgbBlue = Pin(20, Pin.OUT)
 
 # from https://github.com/raspberrypi/pico-micropython-examples/blob/master/blink/blink.py
 
