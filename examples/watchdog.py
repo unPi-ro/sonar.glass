66a57f9f1e780112b3476207d3e0d4c0fa7e9fd4
Ciprian Manea <ciprian.manea@gmail.com>
2021-04-02 16:22:52 +0300

    Update watchdog.py

--- examples/watchdog.py
+++ examples/watchdog.py
@@ -1,6 +1,9 @@
-# just don't do it in main.py, or else BOOTP time again
-
 from machine import WDT
+
+# from https://docs.micropython.org/en/latest/library/machine.WDT.html
+
+# just don't save it as main.py, or else is BOOTP time again
+
 wdt = WDT(timeout=2000)  # enable it with a timeout of 2s
 
 # do more
