72cf1fe8e471d0ef7d7d433b525b7562c1dca0ef
Ciprian Manea <ciprian.manea@gmail.com>
2021-04-03 22:15:18 +0300

    machine ID

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import utime
+import uos
 
 # just a placeholder / demo code
 
 # our Reset/s Time Clock at boot
 with open('main.txt', 'w') as f:
-    f.write("Pico's main.py said Hello World on {}".format(utime.localtime()))
+    f.write("{} at main.py said Hello World on {}".format(uos.uname().machine, utime.localtime()))
 
 # take ur time
 utime.sleep(5)
