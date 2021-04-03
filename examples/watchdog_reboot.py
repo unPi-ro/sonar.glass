b35ea3e50ab8e2ed83d0cc41e1a841e34de6fe38
Ciprian Manea <ciprian.manea@gmail.com>
2021-04-03 10:09:10 +0300

    Update watchdog_reboot.py

--- examples/watchdog_reboot.py
+++ examples/watchdog_reboot.py
@@ -2,7 +2,7 @@ from machine import WDT
 
 # from https://docs.micropython.org/en/latest/library/machine.WDT.html
 
-# just don't save it as main.py, or else is BOOTP time again
+# just don't save it as main.py, or else is BOOTing time forever
 
 wdt = WDT(timeout=2000)  # enable it with a timeout of 2s
 
