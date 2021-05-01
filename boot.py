350901d8e60e9326fa4091153ae345d708e6ebbd
Ciprian Manea <ciprian.manea@gmail.com>
2021-05-01 11:52:23 +0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -9,13 +9,14 @@ led = Pin(25, Pin.OUT)
 LED_state = True
 timer = Timer()
 
+@micropython.native
 def tick(timer):
     global led, LED_state
     LED_state = not LED_state
     led.value(LED_state)
 
 # start flashing onboard led as a booting / setup hint
-timer.init(freq=2, mode=Timer.PERIODIC, callback=tick)
+timer.init(freq=3, mode=Timer.PERIODIC, callback=tick)
 
 # be fast! or your IDE will be slow to connect to Pico if:
 ## utime.sleep(5)
@@ -23,3 +24,4 @@ timer.init(freq=2, mode=Timer.PERIODIC, callback=tick)
 # our Reset/s Time Clock at boot
 with open('boot.txt', 'w') as f:
     f.write("{} at boot.py said Hello World on {}".format(uos.uname().machine, utime.localtime()))
+
